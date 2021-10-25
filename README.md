f02a34b56e7fefe8028d3c84c4b23c2e2cef427d
ahmedbani <a13banisal@gmail.com>
2021-10-25 18:38:02 +0300

    lab 12

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ here is a songs application :
 - if you put in the route `/hello` it will render a page with hello world
 - if you put `/capitalize` and put a text after it, it will capitalize the text you entered 
 - if the route `/album` it will render a page with the albums
+
+- if you put `/form` you will have a form and the user can enter an album it will be stored in the database and after the user submits it will render all the albums that we have 
--- src/main/java/com/example/songr/Models/Album.java
+++ src/main/java/com/example/songr/Models/Album.java
@@ -1,13 +1,25 @@
-package com.example.songr;
+package com.example.songr.Models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Album {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
     private String title;
     private String artist;
     private int songCount;
     private int length;
     private String imageUrl;
 
-    public Album( String title, String artist, int songCount, int length, String imageUrl) {
+    public Album() {
+    }
+
+    public Album(String title, String artist, int songCount, int length, String imageUrl) {
         this.title = title;
         this.artist = artist;
         this.songCount = songCount;
@@ -15,6 +27,9 @@ public class Album {
         this.imageUrl = imageUrl;
     }
 
+    public int getId() {
+        return id;
+    }
 
     public void setTitle(String title) {
         this.title = title;
