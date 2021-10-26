d8f3e2043c85f9613a73ce6d76b7800fb3678444
ahmedbani <a13banisal@gmail.com>
2021-10-26 12:06:14 +0300

    add a test

--- src/test/java/com/example/songr/SongrApplicationTests.java
+++ src/test/java/com/example/songr/SongrApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.songr;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SongrApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Test void testAlbum(){
+		Album newalbum = new Album("Kamikaze","Eminem",10,2749,"https://upload.wikimedia.org/wikipedia/en/6/62/Eminem_-_Kamikaze.jpg");
+		assertEquals("Kamikaze",newalbum.getTitle());
+		assertEquals("Album{title='Kamikaze', artist='Eminem', songCount=10, length=2749, imageUrl='https://upload.wikimedia.org/wikipedia/en/6/62/Eminem_-_Kamikaze.jpg'}",newalbum.toString());
 	}
 
 }
